5773ed463d04a12abef2542abca0366db7b8e25d
Danio Fiore <danio.fiore@sysco.biz>
2023-04-22 18:10:41 +0200

    created the env file and the gitignore

--- app.js
+++ app.js
@@ -8,10 +8,15 @@ const userRouter = require('./routes/userRoutes');
 const app = express();
 
 // 1) MIDDLEWARE
+// we use morgan only if we are in development mode
+if(process.env.NODE_ENV === 'development') {
+  app.use(morgan('dev'));
+}
 app.use(morgan('dev'));
-
 // json is a middleware used to parse the body
 app.use(express.json());
+// this middleware i used for the static file, so now we can access to our html. Under the hood, happens that when we searh in our browser 127.0.0.1:3001/overview.html, express start to search for this url, and when he don't find that, go in the public folder and search for the overview.html file
+app.use(express.static(`${__dirname}/public`));
 
 // even a simple route is a middleware in express, the middlewares are executed in order in the code, so if i put a middleware up here, and don't specify a route, it will be applied to all the route, if we put it after the route (that is a middleware like i said before) it will not work
 app.use((req, res, next) => {
--- server.js
+++ server.js
@@ -1,6 +1,12 @@
 const app = require('./app');
+const dotenv = require('dotenv');
 
-const port = 3001;
+// we installed dotenv that is used to read our config.env file
+dotenv.config({path: './config.env'});
+// used to see our environment variable(global variable)
+// console.log(process.env);
+
+const port = process.env.PORT || 3002;
 app.listen(port, () => {
   console.log(`Running on port: ${port}`);
 });
