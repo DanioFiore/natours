c8a552e700657b20198ec7eb623115b2a89627fa
Danio Fiore <danio.fiore@sysco.biz>
2023-04-25 10:05:32 +0200

    setting up DB connection and mongoose

--- server.js
+++ server.js
@@ -1,3 +1,4 @@
+const mongoose = require('mongoose');
 const app = require('./app');
 const dotenv = require('dotenv');
 
@@ -6,6 +7,19 @@ dotenv.config({path: './config.env'});
 // used to see our environment variable(global variable)
 // console.log(process.env);
 
+const DB = process.env.DATABASE.replace('<PASSWORD>', process.env.DATABASE_PASSWORD);
+
+mongoose
+  .connect(DB, {
+    useNewUrlParser: true,
+    useCreateIndex: true,
+    useFindAndModify: false,
+    useUnifiedTopology: true,
+  })
+  .then(() => {
+    // console.log(connection.connections);
+    console.log('DB connection successfull');
+  });
 const port = process.env.PORT || 3002;
 app.listen(port, () => {
   console.log(`Running on port: ${port}`);
