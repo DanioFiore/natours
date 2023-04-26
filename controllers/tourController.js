02e953ad798d1bdadd71ac6e7462926e63351155
Danio Fiore <danio.fiore@sysco.biz>
2023-04-26 20:09:24 +0200

    finished the tour crud

--- controllers/tourController.js
+++ controllers/tourController.js
@@ -53,10 +53,39 @@ exports.createTour = async (req, res) => {
   
 };
 
-exports.deleteTour = (req, res) => {
-    res.status(404).json({
-        status: 'error',
-        message: 'not available yet'
+exports.updateTour = async (req, res) => {
+  try {
+    const tour = await Tour.findByIdAndUpdate(req.params.id, req.body, {
+      // this will return to te client the new document
+      new: true,
+      // this will run again the validation in our schema
+      runValidators: true,
+    });
+    res.status(200).json({
+      status: 'success',
+      data: {
+        tour
+      }
     })
+  } catch (error) {
+    res.status(404).json({
+      status: 'failed',
+      message: error,
+    });
+  }
+}
+
+exports.deleteTour = async (req, res) => {
+    try {
+      await Tour.findByIdAndDelete(req.params.id);
+      res.status(204).json({
+        status: 'success',
+      })
+    } catch (error) {
+      res.status(404).json({
+        status: 'failed',
+        message: error,
+      });
+    }
 };
 
--- routes/tourRoutes.js
+++ routes/tourRoutes.js
@@ -6,7 +6,7 @@ const router = express.Router();
 // this is a param middleware that it only function for the url with the specified parameter. Here we can also have the val variable that is the value of our parameter
 // router.param('id', tourController.checkID);
 // we can add a parameter to the url like :id and we can make it optional by adding a question mark :id?. We can access to req.params to see the url parameters
-router.route('/:id').get(tourController.getTour).delete(tourController.deleteTour);
+router.route('/:id').get(tourController.getTour).delete(tourController.deleteTour).patch(tourController.updateTour);
 // we can add together the request with the same url
 // router.post('/api/v1/tours', addTour);
 // router.get('/api/v1/tours', getAllTours);
