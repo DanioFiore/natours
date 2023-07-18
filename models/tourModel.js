cea4b147740aeb233c1c7b3968dd585dfcace2d5
Danio Fiore <danio.fiore@sysco.biz>
2023-07-18 09:10:54 +0200

    added some simple validation to tourModel

--- models/tourModel.js
+++ models/tourModel.js
@@ -2,72 +2,85 @@
 const mongoose = require('mongoose');
 const slugify = require('slugify');
 
-const tourSchema = new mongoose.Schema({
-  name: {
-    type: String,
-    required: [true, 'A tour must have a name'],
-    unique: true,
-    trim: true
+const tourSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: [true, 'A tour must have a name'],
+      unique: true,
+      trim: true,
+      maxlength: [40, 'A tour name must have less or equal then 40 characters'],
+      minlength: [10, 'A tour name must have more or equal then 10 characters'],
+    },
+    slug: String,
+    duration: {
+      type: Number,
+      required: [true, 'A tour must have a name'],
+    },
+    maxGroupSize: {
+      type: Number,
+      required: [true, 'A tour must have a group size'],
+    },
+    difficulty: {
+      type: String,
+      required: [true, 'A tour must have a difficulty'],
+      enum: {
+        // * this is the real validator syntax, we can't use the shortand here because we have an enum
+        values: ['easy', 'medium', 'difficult'],
+        message: 'Difficulty is either: easy, medium, difficult'
+      },
+    },
+    ratingsAverage: {
+      type: Number,
+      default: 4.5,
+      min: [1, 'Rating must be above 1.0'],
+      max: [5, 'Rating must be below 5.0'],
+    },
+    ratingsQuantity: {
+      type: Number,
+      default: 0,
+    },
+    price: {
+      type: Number,
+      required: [true, 'A tour must have a price'],
+    },
+    priceDiscount: {
+      type: Number,
+    },
+    summary: {
+      type: String,
+      // trim works only for trim that remove all the white space at the beginning and at the end
+      trim: true,
+      required: [true, 'A tour must have a summary'],
+    },
+    description: {
+      type: String,
+      trim: true,
+      required: [true, 'A tour must have a description'],
+    },
+    imageCover: {
+      // an image is basically a string
+      type: String,
+      required: [true, 'A tour must have an image'],
+    },
+    // like this, we pass an ARRAY OF STRING
+    images: [String],
+    createdAt: {
+      type: Date,
+      default: Date.now(),
+      select: false, // by default we don't send that field to the client
+    },
+    startDates: [Date],
+    secretTour: {
+      type: Boolean,
+      default: false,
+    },
   },
-  slug: String,
-  duration: {
-    type: Number,
-    required: [true, 'A tour must have a name']
-  },
-  maxGroupSize: {
-    type: Number,
-    required: [true, 'A tour must have a group size']
-  },
-  difficulty: {
-    type: String,
-    required: [true, 'A tour must have a difficulty']
-  },
-  ratingsAverage: {
-    type: Number
-  },
-  ratingsQuantity: {
-    type: Number,
-    default: 0
-  },
-  price: {
-    type: Number,
-    required: [true, 'A tour must have a price'],
-  },
-  priceDiscount: {
-    type: Number,
-  },
-  summary: {
-    type: String,
-    // trim works only for trim that remove all the white space at the beginning and at the end
-    trim: true,
-    required: [true, 'A tour must have a summary']
-  },
-  description: {
-    type: String, 
-    trim: true,
-    required: [true, 'A tour must have a description']
-  },
-  imageCover: {
-    // an image is basically a string
-    type: String,
-    required: [true, 'A tour must have an image']
-  },
-  // like this, we pass an ARRAY OF STRING
-  images: [String],
-  createdAt: {
-    type: Date,
-    default: Date.now(),
-    select: false // by default we don't send that field to the client
-  },
-  startDates: [Date],
-  secretTour: {
-    type: Boolean,
-    default: false
+  {
+    toJSON: { virtual: true }, // to effectively see our virtual property. Every time our data will be outputted as json we call virtual
+    toObject: { virtual: true },
   }
-}, {
-  toJSON: { virtual: true }, // to effectively see our virtual property. Every time our data will be outputted as json we call virtual
-  toObject: { virtual: true }
-});
+);
 
 /**
  * we use virtual to have a simply datas that will not be saved into the db, we pass to virtual the name of the data that we want to give
