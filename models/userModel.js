406113cc9e666a9578deeef078c334498b37a529
Danio Fiore <danio.fiore@sysco.biz>
2023-07-20 10:53:22 +0200

    added encryption password

--- models/userModel.js
+++ models/userModel.js
@@ -1,5 +1,6 @@
 const mongoose = require('mongoose');
 const validator = require('validator');
+const bcrypt = require('bcryptjs');
 
 const userSchema = mongoose.Schema({
     name: {
@@ -33,6 +34,17 @@ const userSchema = mongoose.Schema({
         },
     },
 });
+
+userSchema.pre('save', async function(next) {
+    // ONly run this function if password was actually modified
+    if(!this.isModified('password')) return next();
+
+    // encrypt the password, with the second param we indicate how much power the computer will use for this operation. 12 is ok as a cost. More cost = more time but more complexity of the password
+    this.password = await bcrypt.hash(this.password, 12);
+
+    // Delete the password confirm field, we don't need it
+    this.passwordConfirm = undefined;
+})
 
 const User = mongoose.model('User', userSchema);
 
