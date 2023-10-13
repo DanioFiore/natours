da091947251547d17cf9e1594dbd0042012080ff
Danio Fiore <danio.fiore@sysco.biz>
2023-10-13 09:00:25 +0200

    added request limiter and helmet

--- app.js
+++ app.js
@@ -3,20 +3,34 @@ const fs = require('fs');
 const morgan = require('morgan');
 const AppError = require('./utils/appError');
 const globalErrorHandler = require('./controllers/errorController');
+const rateLimit = require('express-rate-limit');
+const helmet = require('helmet');
 
 const tourRouter = require('./routes/tourRoutes');
 const userRouter = require('./routes/userRoutes');
 
 const app = express();
 
-// 1) MIDDLEWARE
+// 1) GLOBAL MIDDLEWARE
+
+// SET SECURITY HEADERS
+app.use(helmet());
+
 // we use morgan only if we are in development mode
 if(process.env.NODE_ENV === 'development') {
   app.use(morgan('dev'));
 }
-app.use(morgan('dev'));
+
+// PUT A REQUEST LIMITER FOR ALL OUR ROUTES. THIS WILL SET A NEW PARAMETER IN OUR REQUESTS HEADERS. IN THIS CASE WE WANT TO LIMIT 100 REQUESTS IN ONE OUR FROM THE SAME IP.
+const limiter = rateLimit({
+  max: 100,
+  windowMs: 60 * 60 * 100,
+  message: 'Too many requests from this IP, try again in one hour!'
+});
+app.use('/api', limiter);
+
 // json is a middleware used to parse the body
-app.use(express.json());
+app.use(express.json({limit: '10kb'}));
 // this middleware i used for the static file, so now we can access to our html. Under the hood, happens that when we searh in our browser 127.0.0.1:3001/overview.html, express start to search for this url, and when he don't find that, go in the public folder and search for the overview.html file
 app.use(express.static(`${__dirname}/public`));
 
